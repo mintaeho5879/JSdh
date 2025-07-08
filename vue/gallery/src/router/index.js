4d37f2518794ab6c73e372b36cb86b010a570301
minbonjwa <minbonjwa@gmail.com>
2025-07-08 18:03:14 +0900

    갤러리야~

---
+++ vue/gallery/src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/join',
+      name: 'join',
+      component: () => import('@/views/Join.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login.vue')
+    },
+  ],
+})
+
+export default router
---
+++ vue/gallery/src/views/Home.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { getItems } from '@/services/itemService';
+import { onMounted } from 'vue';
+
+onMounted(async() => {
+  const res = await getItems();
+    console.log(res.data);
+})
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="mt-5">HELLO, HOME</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
